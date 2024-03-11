5499e5c2b08baeedc5b2b91cacc23137bfc67fc3
mavierafa <mavi.gouveia678@gmail.com>
2024-03-11 11:50:31 -0300

    11/03

---
+++ MaviProjeto/PontodaHistoria.cs
@@ -0,0 +1,15 @@
+ public class PontodaHistoria
+ {
+   public int Id;
+   public string? Texto;
+   public bool TemResposta = false;
+   public string? TextoDaResposta01;
+   public string? TextoDaResposta02;
+   public string? TextoDaResposta03;
+   public int IdLevelResposta01;
+   public int IdLevelResposta02;
+   public int IdLevelResposta03;
+   public bool GameOver = false;
+
+
+ }
--- MaviProjeto/SegundaTela.xaml.cs
+++ MaviProjeto/SegundaTela.xaml.cs
@@ -2,13 +2,76 @@ namespace MaviProjeto;
 
 public partial class SegundaTela : ContentPage
 {
-
-	List<PontodaHistoria. historia = new List<PontodaHistoria>();
+    List<PontodaHistoria> historia = new List<PontodaHistoria>();
+  PontodaHistoria PontodaHistoriaAtual;
 	
 	public SegundaTela()
 	{
 		InitializeComponent();
-	
-	}
+
+	historia.Add(new PontodaHistoria()
+	{
+		Id = 0,
+		Texto = 
+		TemResposta=true,
+		TextodaRespota01=
+		TextodaRespota02=
+		TextodaRespota03=
+		IdLevelResposta01=1,
+		IdLevelResposta02=10,
+		IdLevelResposta02=10,
+	});
+
+    historia.Add(new PontodaHistoria()
+	{
+		Id = 1,
+		Texto = 
+		TemResposta=true,
+		TextodaRespota01=
+		TextodaRespota02=
+		TextodaRespota03=
+		IdLevelResposta01=2,
+		IdLevelResposta02=10,
+		IdLevelResposta02=10,
+	});
+
+	historia.Add(new PontodaHistoria()
+	{
+		Id = 2,
+		Texto = 
+		TemResposta=true,
+		TextodaRespota01=
+		TextodaRespota02=
+		TextodaRespota03=
+		IdLevelResposta01=3,
+		IdLevelResposta02=10,
+		IdLevelResposta02=10,
+	});
+
+	historia.Add(new PontodaHistoria()
+	{
+		Id = 3,
+		Texto = 
+		TemResposta=true,
+		TextodaRespota01=
+		TextodaRespota02=
+		TextodaRespota03=
+		IdLevelResposta01=4,
+		IdLevelResposta02=10,
+		IdLevelResposta02=10,
+	});
+
+	historia.Add(new PontodaHistoria()
+	{
+		Id = 4,
+		Texto = 
+		TemResposta=true,
+		TextodaRespota01=
+		TextodaRespota02=
+		TextodaRespota03=
+		IdLevelResposta01=5,
+		IdLevelResposta02=10,
+		IdLevelResposta02=10,
+	});
 
 }
