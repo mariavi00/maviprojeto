5f71a9d5efbb5b37bfe9c61f5ca91eeefa68c12e
mavierafa <mavi.gouveia678@gmail.com>
2024-03-13 11:48:20 -0300

    13/03

--- MaviProjeto/SegundaTela.xaml.cs
+++ MaviProjeto/SegundaTela.xaml.cs
@@ -12,66 +12,66 @@ public partial class SegundaTela : ContentPage
 	historia.Add(new PontodaHistoria()
 	{
 		Id = 0,
-		Texto = 
+		Texto = "Qual país tem formato de uma bota?",
 		TemResposta=true,
-		TextodaRespota01=
-		TextodaRespota02=
-		TextodaRespota03=
+		TextodaResposta01="Itália",
+		TextodaResposta02="México",
+		TextodaResposta03="Portugal",
 		IdLevelResposta01=1,
 		IdLevelResposta02=10,
-		IdLevelResposta02=10,
+		IdLevelResposta03=10,
 	});
 
     historia.Add(new PontodaHistoria()
 	{
 		Id = 1,
-		Texto = 
+		Texto = "Qual o menor país do mundo?",
 		TemResposta=true,
-		TextodaRespota01=
-		TextodaRespota02=
-		TextodaRespota03=
-		IdLevelResposta01=2,
+		TextodaResposta01="Brasil",
+		TextodaResposta02="Espanha",
+		TextodaResposta03="Vaticano",
+		IdLevelResposta01=10,
 		IdLevelResposta02=10,
-		IdLevelResposta02=10,
+		IdLevelResposta03=2,
 	});
 
 	historia.Add(new PontodaHistoria()
 	{
 		Id = 2,
-		Texto = 
+		Texto = "Qual o maior país do mundo?",
 		TemResposta=true,
-		TextodaRespota01=
-		TextodaRespota02=
-		TextodaRespota03=
+		TextodaResposta01="Russia",
+		TextodaResposta02="Estados Unidos",
+		TextodaResposta03="Canadá",
 		IdLevelResposta01=3,
 		IdLevelResposta02=10,
-		IdLevelResposta02=10,
+		IdLevelResposta03=10,
 	});
 
 	historia.Add(new PontodaHistoria()
 	{
 		Id = 3,
-		Texto = 
+		Texto = "A que temperatura a água ferve?",
 		TemResposta=true,
-		TextodaRespota01=
-		TextodaRespota02=
-		TextodaRespota03=
-		IdLevelResposta01=4,
-		IdLevelResposta02=10,
-		IdLevelResposta02=10,
+		TextodaResposta01="180º",
+		TextodaResposta02="100º",
+		TextodaResposta03="200º",
+		IdLevelResposta01=10,
+		IdLevelResposta02=4,
+		IdLevelResposta03=10,
 	});
 
 	historia.Add(new PontodaHistoria()
 	{
 		Id = 4,
-		Texto = 
+		Texto = "Qual o maior planeta do sistema solar?",
 		TemResposta=true,
-		TextodaRespota01=
-		TextodaRespota02=
-		TextodaRespota03=
-		IdLevelResposta01=5,
+		TextodaResposta01="Marte",
+		TextodaResposta02="Terra",
+		TextodaResposta03="Júpiter",
+		IdLevelResposta01=10,
 		IdLevelResposta02=10,
-		IdLevelResposta02=10,
+		IdLevelResposta03=5,
 	});
 
 	historia.Add(new PontodaHistoria()
@@ -97,6 +97,54 @@ public partial class SegundaTela : ContentPage
 	{
 		TrocaPontodaHistoriaAtual(0);
 	}
+
+	void PreencherPagina()
+	{
+		labelTexto.Text = PontodaHistoriaAtual.Texto;
+
+		if (PontodaHistoriaAtual.GameOver)
+			frameGameOver.IsVisible = true;
+		else
+			frameGameOver.IsVisible = false;
+
+		if (PontodaHistoriaAtual.Ganhou)
+			frameGanhou.IsVisible = true;
+		else
+			frameGanhou.IsVisible = false;
+
+		 (PontodaHistoriaAtual.TemResposta)
+
+		BotaoResposta01.Text = PontodaHistoriaAtual.TextoDaResposta01;
+      	BotaoResposta02.Text = PontodaHistoriaAtual.TextoDaResposta02;
+      	BotaoResposta03.Text = PontodaHistoriaAtual.TextoDaResposta03;
+
+	}
+
+	void TrocaPontodaHistoriaAtual(int id)
+  {
+    PontodaHistoriaAtual = historia.Where(d => d.Id == id).First();
+    PreencherPagina();
+  }
+
+  void Botao01FoiClicado(object sender, EventArgs args)
+  {
+    TrocaPontodaHistoriaAtual(PontodaHistoriaAtual.IdLevelResposta01);
+  }
+
+  void Botao02FoiClicado(object sender, EventArgs args)
+  {
+    TrocaPontodaHistoriaAtual(PontodaHistoriaAtual.IdLevelResposta02);
+  }
+
+   void Botao03FoiClicado(object sender, EventArgs args)
+  {
+    TrocaPontodaHistoriaAtual(PontodaHistoriaAtual.IdLevelResposta03);
+  }
+
+  void BotaoDeVoltarClicado(object sender, EventArgs args)
+  {
+    Iniciar();
+  }
 
 
 }
