1426e90e04dadd92ef4bfba19383c2300f9d2fca
mavierafa <mavi.gouveia678@gmail.com>
2024-03-14 08:47:42 -0300

    14/03

--- MaviProjeto/PontodaHistoria.cs
+++ MaviProjeto/PontodaHistoria.cs
@@ -3,9 +3,9 @@
    public int Id;
    public string? Texto;
    public bool TemResposta = false;
-   public string? TextodaResposta01;
-   public string? TextodaResposta02;
-   public string? TextodaResposta03;
+   public string? TextoDaResposta01;
+   public string? TextoDaResposta02;
+   public string? TextoDaResposta03;
    public int IdLevelResposta01;
    public int IdLevelResposta02;
    public int IdLevelResposta03;
--- MaviProjeto/SegundaTela.xaml.cs
+++ MaviProjeto/SegundaTela.xaml.cs
@@ -9,88 +9,88 @@ public partial class SegundaTela : ContentPage
 	{
 		InitializeComponent();
 
-	historia.Add(new PontodaHistoria()
-	{
-		Id = 0,
-		Texto = "Qual país tem formato de uma bota?",
-		TemResposta=true,
-		TextodaResposta01="Itália",
-		TextodaResposta02="México",
-		TextodaResposta03="Portugal",
-		IdLevelResposta01=1,
-		IdLevelResposta02=10,
-		IdLevelResposta03=10,
-	});
+		historia.Add(new PontodaHistoria()
+		{
+			Id = 0,
+			Texto = "Qual país tem formato de uma bota?",
+			TemResposta=true,
+			TextoDaResposta01="Itália",
+			TextoDaResposta02="México",
+			TextoDaResposta03="Portugal",
+			IdLevelResposta01=1,
+			IdLevelResposta02=10,
+			IdLevelResposta03=10,
+		});
 
-    historia.Add(new PontodaHistoria()
-	{
-		Id = 1,
-		Texto = "Qual o menor país do mundo?",
-		TemResposta=true,
-		TextodaResposta01="Brasil",
-		TextodaResposta02="Espanha",
-		TextodaResposta03="Vaticano",
-		IdLevelResposta01=10,
-		IdLevelResposta02=10,
-		IdLevelResposta03=2,
-	});
+		historia.Add(new PontodaHistoria()
+		{
+			Id = 1,
+			Texto = "Qual o menor país do mundo?",
+			TemResposta=true,
+			TextoDaResposta01="Brasil",
+			TextoDaResposta02="Espanha",
+			TextoDaResposta03="Vaticano",
+			IdLevelResposta01=10,
+			IdLevelResposta02=10,
+			IdLevelResposta03=2,
+		});
 
-	historia.Add(new PontodaHistoria()
-	{
-		Id = 2,
-		Texto = "Qual o maior país do mundo?",
-		TemResposta=true,
-		TextodaResposta01="Russia",
-		TextodaResposta02="Estados Unidos",
-		TextodaResposta03="Canadá",
-		IdLevelResposta01=3,
-		IdLevelResposta02=10,
-		IdLevelResposta03=10,
-	});
+		historia.Add(new PontodaHistoria()
+		{
+			Id = 2,
+			Texto = "Qual o maior país do mundo?",
+			TemResposta=true,
+			TextoDaResposta01="Russia",
+			TextoDaResposta02="Estados Unidos",
+			TextoDaResposta03="Canadá",
+			IdLevelResposta01=3,
+			IdLevelResposta02=10,
+			IdLevelResposta03=10,
+		});
 
-	historia.Add(new PontodaHistoria()
-	{
-		Id = 3,
-		Texto = "A que temperatura a água ferve?",
-		TemResposta=true,
-		TextodaResposta01="180º",
-		TextodaResposta02="100º",
-		TextodaResposta03="200º",
-		IdLevelResposta01=10,
-		IdLevelResposta02=4,
-		IdLevelResposta03=10,
-	});
+		historia.Add(new PontodaHistoria()
+		{
+			Id = 3,
+			Texto = "A que temperatura a água ferve?",
+			TemResposta=true,
+			TextoDaResposta01="180º",
+			TextoDaResposta02="100º",
+			TextoDaResposta03="200º",
+			IdLevelResposta01=10,
+			IdLevelResposta02=4,
+			IdLevelResposta03=10,
+		});
 
-	historia.Add(new PontodaHistoria()
-	{
-		Id = 4,
-		Texto = "Qual o maior planeta do sistema solar?",
-		TemResposta=true,
-		TextodaResposta01="Marte",
-		TextodaResposta02="Terra",
-		TextodaResposta03="Júpiter",
-		IdLevelResposta01=10,
-		IdLevelResposta02=10,
-		IdLevelResposta03=5,
-	});
+		historia.Add(new PontodaHistoria()
+		{
+			Id = 4,
+			Texto = "Qual o maior planeta do sistema solar?",
+			TemResposta=true,
+			TextoDaResposta01="Marte",
+			TextoDaResposta02="Terra",
+			TextoDaResposta03="Júpiter",
+			IdLevelResposta01=10,
+			IdLevelResposta02=10,
+			IdLevelResposta03=5,
+		});
 
-	historia.Add(new PontodaHistoria()
-	{
-		Id = 5,
-		Texto = "Parabéns!! Você acertou todas respostas.",
-		TemResposta=false,
-		Ganhou=true
-	});
+		historia.Add(new PontodaHistoria()
+		{
+			Id = 5,
+			Texto = "Parabéns!! Você acertou todas respostas.",
+			TemResposta=false,
+			Ganhou=true
+		});
 
-	historia.Add(new PontodaHistoria()
-	{
-		Id = 10,
-		Texto = "Você errou!",
-		TemResposta=false,
-		GameOver=true
-	});
+		historia.Add(new PontodaHistoria()
+		{
+			Id = 10,
+			Texto = "Você errou!",
+			TemResposta=false,
+			GameOver=true
+		});
 
-	Iniciar();
+		Iniciar();
 	}
 
 	void Iniciar()
@@ -100,7 +100,7 @@ public partial class SegundaTela : ContentPage
 
 	void PreencherPagina()
 	{
-		labelTexto.Text = PontodaHistoriaAtual.Texto;
+		TextodaLabel.Text = PontodaHistoriaAtual.Texto;
 
 		if (PontodaHistoriaAtual.GameOver)
 			frameGameOver.IsVisible = true;
@@ -112,7 +112,6 @@ public partial class SegundaTela : ContentPage
 		else
 			frameGanhou.IsVisible = false;
 
-		 (PontodaHistoriaAtual.TemResposta)
 
 		BotaoResposta01.Text = PontodaHistoriaAtual.TextoDaResposta01;
       	BotaoResposta02.Text = PontodaHistoriaAtual.TextoDaResposta02;
